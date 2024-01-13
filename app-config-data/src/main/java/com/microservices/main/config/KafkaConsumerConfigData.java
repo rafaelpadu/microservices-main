4c0acf29f711f423e2ec4579dd3204c30e9dde79
Rafael de Padua <rafaelpadua07@gmail.com>
2024-01-13 09:23:33 -0400

    criacao do kafka-consumer

---
+++ app-config-data/src/main/java/com/microservices/main/config/KafkaConsumerConfigData.java
@@ -0,0 +1,27 @@
+package com.microservices.main.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-consumer-config")
+public class KafkaConsumerConfigData {
+    private String keyDeserializer;
+    private String valueDeserializer;
+    private String consumerGroupId;
+    private String autoOffsetRest;
+    private String specificAvroReaderKey;
+    private String specificAvroReader;
+    private Boolean batchListener;
+    private Boolean autoStartup;
+    private Integer concurrencyLevel;
+    private Integer sessionTimeoutMs;
+    private Integer heartBeatIntervalMs;
+    private Integer maxPollIntervalMs;
+    private Integer maxPollRecords;
+    private Integer maxPartitionFetchBytesDefault;
+    private Integer maxPartitionFetchBytesBoostFactor;
+    private Long pollTimeoutMs;
+}
