c0b274cde931817b1a4a1962f050fc16a4c68962
rafael.padua <rafaelpadua07@gmail.com>
2024-01-29 20:22:16 -0400

    deixando a parte de security aberta para rodar a api

---
+++ elastic-query-service/src/main/java/com/microservices/main/elastic/query/service/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.microservices.main.elastic.query.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class WebSecurityConfig {
+//TODO depois corrigir essa parte adicionando a camada de segurança
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) ->
+                        requests.requestMatchers(new AntPathRequestMatcher("/**"))
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
