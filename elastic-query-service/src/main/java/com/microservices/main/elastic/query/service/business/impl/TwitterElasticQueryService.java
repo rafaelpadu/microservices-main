e12d566994610090e340bba92fc360c44b49b682
rafael.padua <rafaelpadua07@gmail.com>
2024-01-30 08:24:45 -0400

    addind hateos for API docs

--- elastic-query-service/src/main/java/com/microservices/main/elastic/query/service/business/impl/TwitterElasticQueryService.java
+++ elastic-query-service/src/main/java/com/microservices/main/elastic/query/service/business/impl/TwitterElasticQueryService.java
@@ -4,6 +4,7 @@ import com.microservices.main.elastic.model.index.impl.TwitterIndexModel;
 import com.microservices.main.elastic.query.client.service.ElasticQueryClient;
 import com.microservices.main.elastic.query.service.business.ElasticQueryService;
 import com.microservices.main.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.main.elastic.query.service.model.assembler.ElasticQueryServiceResponseModelAssembler;
 import com.microservices.main.elastic.query.service.transformer.ElasticToResponseModelTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,30 +15,30 @@ import java.util.List;
 @Service
 public class TwitterElasticQueryService implements ElasticQueryService {
     private static final Logger logger = LoggerFactory.getLogger(TwitterElasticQueryService.class);
-    private final ElasticToResponseModelTransformer elasticToResponseModelTransformer;
+    private final ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler;
 
     private final ElasticQueryClient<TwitterIndexModel> elasticQueryClient;
 
-    public TwitterElasticQueryService(ElasticToResponseModelTransformer elasticToResponseModelTransformer, ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
-        this.elasticToResponseModelTransformer = elasticToResponseModelTransformer;
+    public TwitterElasticQueryService(ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler, ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
+        this.elasticQueryServiceResponseModelAssembler = elasticQueryServiceResponseModelAssembler;
         this.elasticQueryClient = elasticQueryClient;
     }
 
     @Override
     public ElasticQueryServiceResponseModel getDocumentById(String id) {
         logger.info("Queynrg elasticsearch by id{}", id);
-        return elasticToResponseModelTransformer.getResponseModel(elasticQueryClient.getIndexModelById(id));
+        return elasticQueryServiceResponseModelAssembler.toModel(elasticQueryClient.getIndexModelById(id));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getDocumentByText(String text) {
         logger.info("Queryng elasticsearch by text {}", text);
-        return elasticToResponseModelTransformer.getResponseModels(elasticQueryClient.getIndexModelByText(text));
+        return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getIndexModelByText(text));
     }
 
     @Override
     public List<ElasticQueryServiceResponseModel> getAllDocuments() {
         logger.info("Queryng all documents in elasticsearch");
-        return elasticToResponseModelTransformer.getResponseModels(elasticQueryClient.getAllIndexModels());
+        return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getAllIndexModels());
     }
 }
