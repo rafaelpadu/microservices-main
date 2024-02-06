f152ee4075225c53f216030cfc4cddb5b4346e8b
rafael.padua <rafaelpadua07@gmail.com>
2024-02-05 20:11:53 -0400

    insercao de camada de segurança ouath2 com keycloak no elasticqueryservice

--- elastic-query-service/src/main/java/com/microservices/main/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/main/elastic/query/service/config/WebSecurityConfig.java
@@ -1,27 +1,82 @@
 package com.microservices.main.elastic.query.service.config;
 
+import com.microservices.main.elastic.query.service.security.TwitterQueryUserDetailsService;
+import com.microservices.main.elastic.query.service.security.TwitterQueryUserJwtConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebSecurityConfig {
+    private final TwitterQueryUserDetailsService twitterQueryUserDetailsService;
+    private final OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
     @Value("${security.paths-to-ignore}")
     private String[] pathsToIgnore;
-//TODO depois corrigir essa parte adicionando a camada de segurança
+
+    public WebSecurityConfig(TwitterQueryUserDetailsService twitterQueryUserDetailsService, OAuth2ResourceServerProperties oAuth2ResourceServerProperties) {
+        this.twitterQueryUserDetailsService = twitterQueryUserDetailsService;
+        this.oAuth2ResourceServerProperties = oAuth2ResourceServerProperties;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) ->
-                        requests.requestMatchers(new AntPathRequestMatcher("/**"))
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated())
-                .httpBasic(Customizer.withDefaults());
+    public MethodSecurityExpressionHandler expressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        return expressionHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(Arrays.stream(pathsToIgnore).map(AntPathRequestMatcher::new).toList().toArray(new RequestMatcher[]{}))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(twitterQueryUserJwtConverter())));
         return http.build();
     }
+
+    @Bean
+    JwtDecoder jwtDecoder(@Qualifier("elastic-query-service-audience-validator")
+                          OAuth2TokenValidator<Jwt> audienceValidator) {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(
+                oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+        OAuth2TokenValidator<Jwt> withIssuer =
+                JwtValidators.createDefaultWithIssuer(
+                        oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+        OAuth2TokenValidator<Jwt> withAudience =
+                new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
+    }
+
+    @Bean
+    Converter<Jwt, ? extends AbstractAuthenticationToken> twitterQueryUserJwtConverter() {
+        return new TwitterQueryUserJwtConverter(twitterQueryUserDetailsService);
+    }
+
 }
---
+++ elastic-query-service/src/main/java/com/microservices/main/elastic/query/service/security/TwitterQueryUser.java
@@ -0,0 +1,56 @@
+package com.microservices.main.elastic.query.service.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+import static com.microservices.main.elastic.query.service.Constants.NA;
+
+@Builder
+@Getter
+public class TwitterQueryUser implements UserDetails {
+    private String userName;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return NA;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
